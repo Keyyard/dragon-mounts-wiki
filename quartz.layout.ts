17f27563b86de711bc60ba408d3f2ba557dec664
Keyyard <keyyard8888@gmail.com>
2025-06-07 19:18:04 +0700

    fix: update footer links for GitHub and Discord community

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/DragonMounts-Team",
+      "Discord Community": "https://discord.gg/Ewm8aTTJ3K",
     },
   }),
 }
