5286accb14618fee57f21ece783eff8f0421c7e5
Manager Tom <154479323+Tomanex@users.noreply.github.com>
2025-06-08 18:42:37 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Dragons Mount Wiki",
+    pageTitle: "Dragons Mounts",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
